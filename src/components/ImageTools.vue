eb0839b905b5c23eb1621615bc8036d082a821e7
xxss0903 <353768356@qq.com>
2024-09-17 18:18:11 +0800

    feat: imagetools增加自定义模块

--- src/components/ImageTools.vue
+++ src/components/ImageTools.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
+import { inject, computed } from 'vue'
 import ToolItem from './ToolItem.vue'
 import ToolsContainer from './ToolsContainer.vue'
 
 const router = useRouter()
+const route = useRoute()
 
-const imageTools = [
+// 注入 titles 数据
+const titles = inject('titles') as Ref<TitleModule[]>
+
+// 计算当前路由对应的模块
+const currentModule = computed(() => {
+  return titles.value.find((title) => title.value === '/image-tools') || titles.value[0]
+})
+
+// 获取当前模块的子模块
+const customTools = computed(() => {
+  return currentModule.value.children.map((child) => ({
+    name: child.title,
+    route: child.value,
+    image: 'https://img.icons8.com/?size=100&id=12455&format=png&color=000000' // 默认图标，您可以根据需要修改
+  }))
+})
+
+const defaultImageTools = [
   {
     name: '图片压缩',
     route: 'ImageCompressor',
@@ -26,7 +45,6 @@ const imageTools = [
     route: 'ImageCropper',
     image: 'https://img.icons8.com/?size=100&id=qxnheTBF0af8&format=png&color=000000'
   },
-  // 添加Icons8工具
   {
     name: 'Icons8',
     route: 'Icons8Viewer',
@@ -42,16 +60,27 @@ const imageTools = [
     route: 'ScreenshotTool',
     image: 'https://img.icons8.com/?size=100&id=112798&format=png&color=000000'
   },
-   {
+  {
     name: 'Svg渲染',
     route: 'SvgEditor',
     image: 'https://img.icons8.com/?size=100&id=57475&format=png&color=000000'
   }
-  // ... 其他工具 ...
 ]
 
+// 合并默认工具和自定义子模块
+const allImageTools = computed(() => {
+  return [...defaultImageTools, ...customTools.value]
+})
+
 const navigateTo = (routeName: string) => {
-  router.push({ name: routeName })
+  if (routeName.startsWith('custom-')) {
+    const customTool = customTools.value.find((tool) => tool.route === routeName)
+    if (customTool) {
+      window.open(customTool.route, '_blank')
+    }
+  } else {
+    router.push({ name: routeName })
+  }
 }
 
 const goBack = () => {
@@ -62,7 +91,7 @@ const goBack = () => {
 <template>
   <ToolsContainer title="图片工具" @goBack="goBack">
     <ToolItem
-      v-for="tool in imageTools"
+      v-for="tool in allImageTools"
       :key="tool.name"
       :title="tool.name"
       :imageSrc="tool.image"
